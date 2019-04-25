45dd83b9893ee9c7288bf862e7665453a374d586
Eric Walti <ewalti@gmail.com>
2019-04-24 17:59:51 -1000

    build

---
+++ DatePicker/index.d.ts
@@ -0,0 +1,7 @@
+import { DatePickerInlineProps } from './DatePickerInline';
+import { DatePickerModalProps } from './DatePickerModal';
+export declare type DatePickerProps = DatePickerModalProps;
+export declare type DatePickerInlineProps = DatePickerInlineProps;
+export { default as DatePickerBase } from './DatePicker';
+export { default } from './DatePickerModal';
+export { default as InlineDatePicker } from './DatePickerInline';
---
+++ DatePicker/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var DatePicker_1 = require("./DatePicker");
+exports.DatePickerBase = DatePicker_1.default;
+var DatePickerModal_1 = require("./DatePickerModal");
+exports.default = DatePickerModal_1.default;
+var DatePickerInline_1 = require("./DatePickerInline");
+exports.InlineDatePicker = DatePickerInline_1.default;
